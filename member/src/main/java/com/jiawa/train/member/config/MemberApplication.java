e64aa74b5ca866ee6e213999ba901d36fe832a13
davie-oss <lliuqing93@gmail.com>
2024-02-21 12:43:22 +0800

    修改启动日志，增加/member前缀

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,9 +1,12 @@
 package com.jiawa.train.member.config;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 
 @ComponentScan("com.jiawa")
@@ -11,8 +14,15 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class MemberApplication {
 
+
+	private static final Logger log=LoggerFactory.getLogger(MemberApplication.class);
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
+		SpringApplication app = new SpringApplication(MemberApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+		log.info("启动成功");
+		log.info("地址：\thttp;//127.0.0.1:{}",env.getProperty("server.port"));
 	}
 
 }
