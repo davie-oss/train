2702c27dd9df040f2c683f94656894e39253aa04
davie-oss <lliuqing93@gmail.com>
2024-02-24 00:26:27 +0800

    不写入堆栈数据

--- common/src/main/java/com/jiawa/common/exception/BusinessException.java
+++ common/src/main/java/com/jiawa/common/exception/BusinessException.java
@@ -15,4 +15,7 @@ public class BusinessException extends  RuntimeException{
     public void setE(BusinessExceptionEnum e) {
         this.e = e;
     }
+
+    @Override
+    public Throwable fillInStackTrace(){return this;};
 }
