5cb895d45cb5766a4f0469047869ae5ff399d7f3
davie-oss <lliuqing93@gmail.com>
2024-02-23 23:41:02 +0800

    封装请求参数和返回结果

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.jiawa.train.member.controller;
 
+import com.jiawa.common.resp.CommonResp;
+import com.jiawa.train.member.req.MemberRegisterReq;
 import com.jiawa.train.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +18,19 @@ public class MemberController {
     private MemberService memberService;
 
 @GetMapping("/count")
-    public int count() {
-        return memberService.count();
+    public CommonResp<Integer> count() {
+    int count = memberService.count();
+    CommonResp<Integer> commonResp = new CommonResp<>();
+    commonResp.setContent(count);
+
+    return commonResp ;
     }
 
     @PostMapping("/register")
-    public long register(String mobile) {
-        return memberService.register(mobile);
+    public CommonResp<Long> register(MemberRegisterReq req) {
+        long register = memberService.register(req);
+        CommonResp<Long> commonResp = new CommonResp<>();
+        commonResp.setContent(register);
+        return commonResp;
     }
 }
