206eda454e40084f974158f60e996c171ab819c8
davie-oss <lliuqing93@gmail.com>
2024-03-02 15:46:14 +0800

    制作欢迎页面，增加乘车人管理页面，修改菜单，实现页面切换，实现菜单同步激活

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -11,26 +11,65 @@
         退出登录
       </router-link>
     </div>
-
+    <a-menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined /> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined /> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/ticket">
+        <router-link to="/ticket">
+          <border-outer-outlined /> &nbsp; 余票查询
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/my-ticket">
+        <router-link to="/my-ticket">
+          <idcard-outlined /> &nbsp; 我的车票
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/seat">
+        <router-link to="/seat">
+          <usergroup-add-outlined /> &nbsp; 座位销售图
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin">
+        <router-link to="/admin">
+          <desktop-outlined /> &nbsp; 关于控台管理
+        </router-link>
+      </a-menu-item>
+    </a-menu>
   </a-layout-header>
 </template>
 
 <script>
-import {defineComponent, ref} from 'vue';
+import {defineComponent, ref, watch} from 'vue';
 import store from "@/store";
-
-
+import router from '@/router'
 
 export default defineComponent({
   name: "the-header-view",
   setup() {
-      let member = store.state.member;
+    let member = store.state.member;
+    const selectedKeys = ref([]);
 
-
-
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true});
     return {
-      selectedKeys1: ref(['2']),
-      member
+      member,
+      selectedKeys
     };
   },
 });
@@ -38,5 +77,11 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.logo {
+  float: left;
+  height: 31px;
+  width: 150px;
+  color: white;
+  font-size: 20px;
+}
 </style>
