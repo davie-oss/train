1ec38253a1ec7750ea7a79958f42ed6f8cb4bfc0
davie-oss <lliuqing93@gmail.com>
2024-03-03 18:06:09 +0800

    使用passenger模板生成seivice 成功

--- generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/server/ServerGenerator.java
@@ -1,20 +1,22 @@
 package com.jiawa.train.generator.server;
 
+import com.jiawa.train.generator.util.Field;
+import com.jiawa.train.generator.util.FreemarkerUtil;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ServerGenerator {
-    static String toPath = "generator\\src\\main\\java\\com\\jiawa\\train\\generator\\test\\";
+
+    static String serverPath = "[module]/src/main/java/com/jiawa/train/[module]/service/";
     static String pomPath = "generator/pom.xml";
 
     static {
-        new File(toPath).mkdirs();
+        new File(serverPath).mkdirs();
     }
 
 
@@ -23,14 +25,45 @@ public class ServerGenerator {
 //        Map<String, Object> param = new HashMap<>();
 //        param.put("domain", "Test");
 //        FreemarkerUtil.generator(toPath + "Test.java", param);
-
+//      获取mybatis-generator
         String generatorPath = getGeneratorPath();
+        // 比如generator-config-member.xml，得到module = member
+         String module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
+        System.out.println("module: " + module);
+        serverPath = serverPath.replace("[module]", module);
+        new File(serverPath).mkdirs();
+        System.out.println("servicePath: " + serverPath);
+
+
+        //读取table节点
         Document document = new SAXReader().read("generator/" + generatorPath);
         Node table = document.selectSingleNode("//table");
         System.out.println(table);
         Node tableName = table.selectSingleNode("@tableName");
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+
+
+        // 示例：表名 jiawa_test
+        // Domain = JiawaTest
+        String Domain = domainObjectName.getText();
+        // domain = jiawaTest
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        // do_main = jiawa-test
+        String do_main = tableName.getText().replaceAll("_", "-");
+
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("module", module);
+        param.put("Domain", Domain);
+        param.put("domain", domain);
+        param.put("do_main", do_main);
+        System.out.println("组装参数：" + param);
+
+
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(serverPath+Domain+"Service.java",param);
 
     }
 
@@ -43,5 +76,14 @@ public class ServerGenerator {
         Node node = document.selectSingleNode("//pom:configurationFile");
         System.out.println(node.getText());
         return node.getText();
+    }
+
+    private static Set<String> getJavaTypes(List<Field> fieldList) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < fieldList.size(); i++) {
+            Field field = fieldList.get(i);
+            set.add(field.getJavaType());
+        }
+        return set;
     }
 }
