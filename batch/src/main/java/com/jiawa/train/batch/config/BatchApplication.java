90b6192716c86c2d5fc2830ac10f493d11f13cc0
davie-oss <lliuqing93@gmail.com>
2024-03-11 22:53:36 +0800

    增加batch定时调度模块

---
+++ batch/src/main/java/com/jiawa/train/batch/config/BatchApplication.java
@@ -0,0 +1,29 @@
+package com.jiawa.train.batch.config;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+
+@ComponentScan("com.jiawa")
+@MapperScan("com/jiawa/train/batch/mapper")
+@SpringBootApplication
+public class BatchApplication {
+
+
+	private static final Logger log=LoggerFactory.getLogger(BatchApplication.class);
+
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(BatchApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+		log.info("启动成功");
+		log.info("地址：\thttp;//127.0.0.1:{}",env.getProperty("server.port"));
+	}
+
+}
