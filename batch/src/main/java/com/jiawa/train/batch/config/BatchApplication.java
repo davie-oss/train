d7e2a785dcc0c5fccd108730ced7ef6a7e1d2abd
davie-oss <lliuqing93@gmail.com>
2024-03-24 23:40:42 +0800

    为business增加日志拦截器

--- batch/src/main/java/com/jiawa/train/batch/config/BatchApplication.java
+++ batch/src/main/java/com/jiawa/train/batch/config/BatchApplication.java
@@ -6,12 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 
 @ComponentScan("com.jiawa")
 @MapperScan("com/jiawa/train/batch/mapper")
+@EnableFeignClients("com.jiawa.train.batch.feign")
 @SpringBootApplication
 public class BatchApplication {
 
